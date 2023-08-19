c3546f40cdbecd9d00feea8809fab0005703a009
r4uls <140269359+r4uls@users.noreply.github.com>
2023-08-19 16:46:03 -0300

    template da aula
    .

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here
 
-# Create your models here.
+class Aluno(models.Model):
+
+    nome = models.CharField(max_length=254)
+    telefone = models.CharField(null = True,blank=True,max_length=15)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nome
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
+from .models import Aluno
+from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
+
+def alunoView(request):
+    alunos_list = Aluno.objects.all()
+    return render(request, 'main/alunos.html', {'alunos_list': alunos_list})
+
+
