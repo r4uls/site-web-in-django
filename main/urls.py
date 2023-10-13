c572b28506e9529a21b154a76ce5a82e29a3356b
r4uls <140269359+r4uls@users.noreply.github.com>
2023-10-13 16:55:00 -0300

    att

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.alunoView, name='aluno_lista')
+    path('', views.alunoView, name='aluno_lista'),
+
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno_detalhe'),
+
+    path('contact', views.contact_view, name='aluno-contact'),
+
+    path('aluno/create/', AlunoCreateView.as_view(), name='aluno-create'),
+    path('aluno/<int:pk>/update/', AlunoUpdateView.as_view(), name='aluno-update'),
+
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,29 @@
 
 from .models import Aluno
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def alunoView(request):
     alunos_list = Aluno.objects.all()
     return render(request, 'main/alunos.html', {'alunos_list': alunos_list})
 
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request, 'main/alunoID.html', {'aluno':aluno})
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from .forms import AlunoForm
+
+class AlunoCreateView(CreateView):
+    model = Aluno
+    form_class = AlunoForm
+    success_url = reverse_lazy('aluno-lista') #url pra direcionar dps da criaçao do objeto.
+    template_name = 'main/aluno_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
